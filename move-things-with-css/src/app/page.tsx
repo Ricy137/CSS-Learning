8c89746f5bd6952e551175a7218387a3d90a59e6
Ricy <ricyzhang951@gmail.com>
2024-08-09 22:57:37 -0700

    feat: bunny animations

--- move-things-with-css/src/app/page.tsx
+++ move-things-with-css/src/app/page.tsx
@@ -1,15 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Image
-                className=" hover:animate-cvalish_cspin"
-                src="./vercel.svg"
-                alt="next"
-                width={40}
-                height={40}
-            />
+        <main className="flex min-h-screen flex-row items-center justify-center p-24">
+            <div className="bunny" />
+            <div className="bunny" />
+            <div className="bunny" />
+            <div className="bunny" />
+            <div className="bunny" />
         </main>
     );
 }
